8d4d244e18bcaf633f865b88b53338644518f3df
linlinzzo <79007796+linlinzzo@users.noreply.github.com>
2021-02-13 20:43:13 +0800

    Create footer(转cdn库的脚本引用).php

---
+++ footer(转cdn库的脚本引用).php
@@ -0,0 +1,4 @@
+<script type="text/javascript" language="javascript" src="https://cdn.bootcdn.net/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script type="text/javascript" language="javascript" src="https://www.layuicdn.com/layer-mobile-v2.0/layer.js"></script>
+<script type="text/javascript" language="javascript" src="assets/js/ajax.js?v=4.1"></script>
+<?php if(ADMIN) echo '<script type="text/javascript" language="javascript" src="assets/js/admin.js?v=4.0"></script>';?>
