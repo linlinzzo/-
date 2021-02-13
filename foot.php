bc4d2f0c103a6630d52486b0e0cd4d19b1661678
linlinzzo <79007796+linlinzzo@users.noreply.github.com>
2021-02-13 21:04:06 +0800

    Update foot.php

--- foot.php
+++ foot.php
@@ -1,4 +1,6 @@
+<!-- 此处两个脚本引用外部库 -->
 <script type="text/javascript" language="javascript" src="https://cdn.bootcdn.net/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script type="text/javascript" language="javascript" src="https://www.layuicdn.com/layer-mobile-v2.0/layer.js"></script>
+<!-- 这两个脚本外部库找不到，引用本地库 -->
 <script type="text/javascript" language="javascript" src="assets/js/ajax.js?v=4.1"></script>
 <?php if(ADMIN) echo '<script type="text/javascript" language="javascript" src="assets/js/admin.js?v=4.0"></script>';?>
